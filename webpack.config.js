39f3f8ff8f68904268d3ad61cf1d16ca4514872b
givadi <givadi@mail.ru>
2021-02-18 20:18:53 +0300

    Added proposal-babel

--- webpack.config.js
+++ webpack.config.js
@@ -92,6 +92,7 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties'],
           },
         },
       },
